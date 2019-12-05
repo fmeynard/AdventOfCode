ea6ebb3b287480dec46c5be4398a3dc28c2869e6
Fabien MEYNARD <fmeynard@marinsoftware.com>
2019-12-05 01:24:04 +0000

    Fix small bug + opti

--- 2019/p4.js
+++ 2019/p4.js
@@ -1,28 +1,22 @@
-function process(start , end, validationFunction) {
+function process(start , end, callBack) {
     let count = 0;
     for (let i = start; i <= end; i++) {
-        let values = i.toString().split('');
-        let lowerThan = false;
-        let sizes = [];
-        for (let j = 0; j < values.length - 1; j++) {
-            if (values[j] > values[j+1]) {
-                lowerThan = true;
-                break;
+        let values = i.toString().split('').sort();
+        if (values.join('') == i.toString()) {
+            let sizes = [... new Set(values)].map(int => { // [... new Set(values)] <=> array_unique
+                return { int, val: i.toString().split(int).length - 1}
+            });
+    
+            if (sizes.find(curr => curr.val >= 2) && (!callBack || callBack(sizes))) {
+                count++;
             }
-            sizes.push({ int: values[j], val: i.split(values[j]).length - 1});
-        }
-
-        if (!lowerThan && validationFunction(sizes)) {
-            count++;
         }
     }
 
     return count;
 }
 
-console.log({ 
-    part1: process(134792,675810, (sizes) => sizes.find(curr => curr.val >= 2)),
-    part2: process(134792,675810, (sizes) => {
-        return sizes.find(curr => curr.val >= 2) && !(sizes.find(curr => curr.val > 2) && !sizes.find(curr => curr.val == 2));
-    }) 
+console.table({ 
+    part1: process(134792,675810),
+    part2: process(134792,675810, (sizes) => !(sizes.find(curr => curr.val > 2) && !sizes.find(curr => curr.val == 2)))
 });
