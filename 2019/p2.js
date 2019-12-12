408719ef85364ac769107678b253e8f40baa562e
Fabien MEYNARD <fmeynard@marinsoftware.com>
2019-12-12 23:34:27 +0000

    Day 2&5 refactor to use IntCodeComputer class

--- 2019/p2.js
+++ 2019/p2.js
@@ -1,40 +1,32 @@
 const fs = require('fs');
+const IntCodeComputer = require('./int-code-computer');
 
 function process(input, noun, verb) {
     input[1] = noun;
     input[2] = verb;
 
-    let x = 0;
-    while(input[x] != 99) {
-        if (input[x] == 1) {
-            input[input[x + 3]] = input[input[x + 1]] + input[input[x + 2]];
-        } else if (input[x] == 2) {
-            input[input[x + 3]] = input[input[x + 1]] * input[input[x + 2]];
-        }
-        x += 4;
-    }
+    let program = new IntCodeComputer(input);
+    program.run();
 
-    return input[0];
+    return program.memory[0];
 }
 
-function part1() {
-    console.log({ part1: process(fs.readFileSync('./inputs/p2.txt', 'utf-8').split(',').map(val => parseInt(val)), 12, 2)});
-}
-
-function part2() {
-    let input = fs.readFileSync('./inputs/p2.txt', 'utf-8').split(',').map(val => parseInt(val));
-
+function part2(input) {
     let noun = 0, verb = 0;
-    while (process([...input], noun,verb) != 19690720) {
+    while (process(input, noun,verb) != 19690720) {
         noun++;
         if (noun > 99) {
             noun = 0;
             verb++;
         }
     }
-    
-    console.log({ part2: 100 * noun + verb });
+
+    return 100 * noun + verb;
 }
 
-part1();
-part2();
+let input = fs.readFileSync('./inputs/p2.txt', 'utf-8').split(',').map(Number);
+
+console.log({
+    part1: process(input, 12, 2),
+    part2: part2(input)
+});
--- 2019/p5.js
+++ 2019/p5.js
@@ -1,54 +1,9 @@
 const fs = require('fs');
 
-function process(input, programInput) {
-    let x = 0;
-    let output = [];
-    while(input[x] != 99) {
-        input[x] = input[x].toString().padStart(5, '0');
+const IntCodeComputer = require('./int-code-computer');
+let input = fs.readFileSync('./inputs/p5.txt', 'utf-8').split(',').map(Number);
 
-        let p1Index = Number(input[x].substr(-3,1)) == 0 ? input[x + 1] : x + 1;
-        let p2Index = Number(input[x].substr(-4,1)) == 0 ? input[x + 2] : x + 2;
-        let p3Index = Number(input[x].substr(-5,1)) == 0 ? input[x + 3] : x + 3;
-
-        switch (Number(input[x].substr(-2))) {
-            case 1:
-                input[p3Index] = input[p1Index] + input[p2Index];
-                x += 4;
-                break;
-            case 2:
-                input[p3Index] = input[p1Index] * input[p2Index];
-                x += 4;
-                break;
-            case 3:
-                input[p1Index] = programInput;
-                x += 2;
-                break;
-            case 4:
-                output.push(input[p1Index]);
-                x += 2;
-                break;
-            case 5:
-                x = input[p1Index] != 0 ? input[p2Index] : x+3;
-                break;
-            case 6:
-                x = input[p1Index] == 0 ? input[p2Index] : x+3;
-                break;
-            case 7:
-                input[p3Index] = input[p1Index] < input[p2Index] ? 1 : 0;
-                x += 4;
-                break;
-            case 8:
-                input[p3Index] = input[p1Index] == input[p2Index] ? 1 : 0;
-                x += 4;
-                break;
-        }
-    }
-
-    return output.join(' ');
-}
-
-let input = fs.readFileSync('./inputs/p5.txt', 'utf-8').split(',').map(val => parseInt(val));
 console.log({
-    part1: process([...input], 1),
-    part2: process([...input], 5)
+    part1: (new IntCodeComputer([...input], [1])).run().pop(),
+    part2: (new IntCodeComputer([...input], [5])).run().pop()
 });
